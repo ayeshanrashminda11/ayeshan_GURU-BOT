0ca60261dad13e791aa9d81d0cace99e886f510e
ayeshanrashminda11 <152684352+ayeshanrashminda11@users.noreply.github.com>
2023-12-04 17:30:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ayeshan_®'
+global.botname = 'ayeshan_'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
